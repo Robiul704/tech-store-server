63be55f48a270af70987eed56d1785a0f8773560
robiul704 <robiulislam29935@gmail.com>
2023-10-20 19:45:14 +0600

    update the product informtion collection is done

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ async function run() {
       const result=await finding.toArray()
       res.send(result)
     })
-    
     app.delete('/mycart/:id',async(req,res)=>{
       const id=req.params.id
       const query={_id:new ObjectId(id)}
